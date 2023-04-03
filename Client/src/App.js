81b7cc7ef959114d21f6e01cdd60d08979b53543
Akif Syuleyman <f17.8oy@gmail.com>
2023-04-03 23:10:05 +0300

    Added router library and paths to the React app

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Fabric from "./Components/Fabric/Fabric";
+import FabricDetails from "./Components/Fabric/FabricDetails/FabricDetails";
+import Stones from "./Components/Stones/Stones";
+import StonesDetails from "./Components/Stones/StonesDetails/StonesDetails";
+import Stamps from "./Components/Stamps/Stamps";
+import StampDetails from "./Components/Stamps/StampDetails/StampDetails";
+import Clothes from "./Components/Clothes/Clothes";
+import ClothesDetails from "./Components/Clothes/ClothesDetails/ClothesDetails";
+import About from "./Components/About/About";
+import FAQ from "./Components/FAQ/FAQ";
+import Cart from "./Components/Cart/Cart";
+import Profile from "./Components/Profile/Profile";
+import Favorites from "./Components/Favorites/Favorites";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Logout from "./Components/Logout/Logout";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/* HEADER */}
+            <Header />
+            {/* MAIN CONTENT */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/fabric" element={<Fabric />} />
+                <Route path="/fabric/:id" element={<FabricDetails />} />
+                <Route path="/stones" element={<Stones />} />
+                <Route path="/stones/:id" element={<StonesDetails />} />
+                <Route path="/stamps" element={<Stamps />} />
+                <Route path="/stamps/:id" element={<StampDetails />} />
+                <Route path="/clothes" element={<Clothes />} />
+                <Route path="/clothes/:id" element={<ClothesDetails />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/faq" element={<FAQ />} />
+                {/* USER ONLY */}
+                <Route path="/users/profile" element={<Profile />} />
+                <Route path="/users/profile/favorites" element={<Favorites />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/users/logout" element={<Logout />} />
+                {/* GUEST ONLY */}
+                <Route path="/users/login" element={<Login />} />
+                <Route path="/users/register" element={<Register />} />
+            </Routes>
+            {/* FOOTER */}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
