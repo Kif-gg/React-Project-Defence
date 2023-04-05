88632f0e7ed6fc8795e96f94bcdc153abf80c532
Akif Syuleyman <f17.8oy@gmail.com>
2023-04-05 23:27:11 +0300

    Updated fabricService

--- Client/src/Services/fabricService.js
+++ Client/src/Services/fabricService.js
@@ -2,12 +2,23 @@ import * as request from './requester';
 
 const url = 'http://localhost:3030/fabric';
 
-export const getFabricProducts = async () => {
-    const result = await request.get(url);
+export const getFabricProducts = async (searchForm) => {
+    if (searchForm === undefined) {
+        const result = await request.get(url);
+        return result;
+    }
+
+    const { search, extras, sort, direction } = searchForm;
+    const fabricType = searchForm['fabric-type'];
+
+    const result = await request.get(`${url}?search=${search}&fabric-type=${fabricType}&extras=${extras}&sort=${sort}&direction=${direction}`);
     return result;
 };
 
 export const getFabricDetails = async (id) => {
-    const result = await request.get(`${url}/${id}`);
-    return result;
+    return await request.get(`${url}/${id}`);
 };
+
+export const createReview = async (reviewData, productId) => {
+    return await request.post(`${url}/${productId}`, reviewData);
+}
