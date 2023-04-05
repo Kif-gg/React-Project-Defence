1871ff8337e5fbf81672ea07b6eb03cd513ce2d1
Akif Syuleyman <f17.8oy@gmail.com>
2023-04-05 23:26:49 +0300

    Added images to header and check for user

--- Client/src/Components/Header/Header.js
+++ Client/src/Components/Header/Header.js
@@ -1,35 +1,52 @@
+import logo from './Assets/favicon.jpg';
+import name from './Assets/company-name.jpg';
+
 import { Link } from "react-router-dom"
+import { useContext } from "react";
+
+import { AuthContext } from "../../Contexts/AuthContext";
 
 export default function Header() {
+
+    const { isAuthenticated } = useContext(AuthContext)
+
     return (
-        <header>
-            <div className="logo">
-                <img src="../../Resources-images/favicon.jpg" alt="logo" />
-                <h1>Fashion and clothing for everyone</h1>
-                <img src="../../Resources-images/company-name.jpg" alt="name" />
-            </div>
-            <nav>
-                <ul>
-                    <div className="left">
-                        <li><Link to="/">Home page</Link></li>
-                        <li><Link to="/fabric">Fabric</Link></li>
-                        <li><Link to="/stones">Stones for stamps</Link></li>
-                        <li><Link to="/stamps">Stamp designs</Link></li>
-                        <li><Link to="/clothes">Clothes</Link></li>
-                        <li><Link to="/about">About us</Link></li>
-                        <li><Link to="/faq">FAQ</Link></li>
-                    </div>
-                    <div className="right">
+        <div id='header'>
+            <header>
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                    <h1>Fashion and clothing for everyone</h1>
+                    <img src={name} alt="name" />
+                </div>
+                <nav>
+                    <ul>
+                        <div className="left">
+                            <li><Link to="/">Home page</Link></li>
+                            <li><Link to="/fabric">Fabric</Link></li>
+                            <li><Link to="/stones">Stones for stamps</Link></li>
+                            <li><Link to="/stamps">Stamp designs</Link></li>
+                            <li><Link to="/clothes">Clothes</Link></li>
+                            <li><Link to="/about">About us</Link></li>
+                            <li><Link to="/faq">FAQ</Link></li>
+                        </div>
                         {/* LOGGED USERS */}
-                        <li><Link to="/users/profile">Profile</Link></li>
-                        <li><Link to="/cart">Cart</Link></li>
-                        <li><Link to="/users/logout">Log out</Link></li>
+                        {isAuthenticated && (
+                            <div className="right">
+                                <li><Link to="/users/profile">Profile</Link></li>
+                                <li><Link to="/cart">Cart</Link></li>
+                                <li><Link to="/users/logout">Log out</Link></li>
+                            </div>
+                        )}
                         {/* GUESTS */}
-                        <li><Link to="/users/login">Log in</Link></li>
-                        <li><Link to="/users/register">Register</Link></li>
-                    </div>
-                </ul>
-            </nav>
-        </header>
+                        {!isAuthenticated && (
+                            <div className="right">
+                                <li><Link to="/users/login">Log in</Link></li>
+                                <li><Link to="/users/register">Register</Link></li>
+                            </div>
+                        )}
+                    </ul>
+                </nav>
+            </header >
+        </div>
     );
 };
