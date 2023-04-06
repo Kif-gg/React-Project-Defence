832f1b924fe516cfa5b4cdb94d27c7a4e3aad9c8
Akif Syuleyman <f17.8oy@gmail.com>
2023-04-06 08:36:52 +0300

    Added edit and delete functions

--- Client/src/Services/authService.js
+++ Client/src/Services/authService.js
@@ -3,26 +3,29 @@ import * as request from './requester';
 const url = 'http://localhost:3030/users';
 
 export const login = async (loginData) => {
-    const result = await request.post(`${url}/login`, loginData);
-    return result;
+    return await request.post(`${url}/login`, loginData);
 };
 
 export const register = async (registerData) => {
-    const result = await request.post(`${url}/register`, registerData);
-    return result;
+    return await request.post(`${url}/register`, registerData);
 };
 
 export const logout = async () => {
-    const result = await request.get(`${url}/logout`);
-    return result;
+    return await request.get(`${url}/logout`);
 }
 
 export const getUserProfile = async () => {
-    const result = await request.get(`${url}/profile`);
-    return result;
+    return await request.get(`${url}/profile`);
 };
 
 export const getUserFavorites = async () => {
-    const result = await request.get(`${url}/profile/favorites`);
-    return result;
+    return await request.get(`${url}/profile/favorites`);
 };
+
+export const editUserData = async (editedData) => {
+    return await request.put(`${url}/profile`, editedData);
+};
+
+export const deleteUserProfile = async (password) => {
+    return await request.del(`${url}/profile`, password)
+}
