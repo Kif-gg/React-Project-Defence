e497d86a8a2a5784e343d625696cd595b6b42302
Akif Syuleyman <f17.8oy@gmail.com>
2023-04-08 09:10:10 +0300

    Added toggle display functionality

--- Client/src/Components/FAQ/FAQ.js
+++ Client/src/Components/FAQ/FAQ.js
@@ -1,54 +1,66 @@
 export default function FAQ() {
+
+    const toggleAccordion = (e) => {
+        e.currentTarget.nextSibling.classList.toggle('answer-shown');
+        e.currentTarget.nextSibling.classList.toggle('answer-hidden');
+        const chevrons = e.currentTarget.getElementsByTagName('I');
+
+        for (let chevron of chevrons) {
+            chevron.classList.toggle('fa-circle-chevron-down');
+            chevron.classList.toggle('fa-circle-chevron-up');
+        }
+    };
+
     return (
         <div id="faq">
             <main>
                 <h2>FAQ Section</h2>
                 <section>
                     <div className="qa-block">
-                        <div className="question">
+                        <div className="question" onClick={toggleAccordion}>
                             <i className="fa-solid fa-circle-chevron-down"></i><p>Question</p><i className="fa-solid fa-circle-chevron-down"></i>
                         </div>
-                        <div className="answer">
+                        <div className="answer-hidden">
                             <p>Answer</p>
                         </div>
                     </div>
                     <div className="qa-block">
-                        <div className="question">
+                        <div className="question" onClick={toggleAccordion}>
                             <i className="fa-solid fa-circle-chevron-down"></i><p>Question</p><i className="fa-solid fa-circle-chevron-down"></i>
                         </div>
-                        <div className="answer">
+                        <div className="answer-hidden">
                             <p>Answer</p>
                         </div>
                     </div>
                     <div className="qa-block">
-                        <div className="question">
+                        <div className="question" onClick={toggleAccordion}>
                             <i className="fa-solid fa-circle-chevron-down"></i><p>Question</p><i className="fa-solid fa-circle-chevron-down"></i>
                         </div>
-                        <div className="answer">
+                        <div className="answer-hidden">
                             <p>Answer</p>
                         </div>
                     </div>
                     <div className="qa-block">
-                        <div className="question">
+                        <div className="question" onClick={toggleAccordion}>
                             <i className="fa-solid fa-circle-chevron-down"></i><p>Question</p><i className="fa-solid fa-circle-chevron-down"></i>
                         </div>
-                        <div className="answer">
+                        <div className="answer-hidden">
                             <p>Answer</p>
                         </div>
                     </div>
                     <div className="qa-block">
-                        <div className="question">
+                        <div className="question" onClick={toggleAccordion}>
                             <i className="fa-solid fa-circle-chevron-down"></i><p>Question</p><i className="fa-solid fa-circle-chevron-down"></i>
                         </div>
-                        <div className="answer">
+                        <div className="answer-hidden">
                             <p>Answer</p>
                         </div>
                     </div>
                     <div className="qa-block">
-                        <div className="question">
+                        <div className="question" onClick={toggleAccordion}>
                             <i className="fa-solid fa-circle-chevron-down"></i><p>Question</p><i className="fa-solid fa-circle-chevron-down"></i>
                         </div>
-                        <div className="answer">
+                        <div className="answer-hidden">
                             <p>Answer</p>
                         </div>
                     </div>
